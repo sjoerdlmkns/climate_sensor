e7d23f644b5a9d7f8be24a240541ffea698b3b36
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-02-04 19:35:17 +0100

    feat: connect to mqtt broker

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <DHT.h>
 #include <ESP8266WiFi.h>
 #include <Ticker.h>
+#include <AsyncMqttClient.h>
 
 // DHT Sensor
 #define DHT_BUS 4
@@ -12,10 +13,31 @@ DHT dht(DHT_BUS, DHT_TYPE);
 #define WIFI_SSID ""
 #define WIFI_PASSWORD ""
 
+// MQTT Broker settings
+#define MQTT_HOST IPAddress(0, 0, 0, 0)
+#define MQTT_PORT 1883
+#define MQTT_CLIENT_ID ""
+#define MQTT_USERNAME ""
+#define MQTT_PASSWORD ""
+
+// MQTT Topics
+#define TOPIC "/sensor"
+
 // Wifi managers
 WiFiEventHandler wifiConnectHandler;
 WiFiEventHandler wifiDisconnectHandler;
+
+// Tickers
 Ticker wifiReconnectTimer;
+Ticker mqttReconnectTimer;
+
+AsyncMqttClient mqttClient;
+
+void connectToMqtt()
+{
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
 
 void connectToWifi()
 {
@@ -26,12 +48,39 @@ void connectToWifi()
 void onWifiConnect(const WiFiEventStationModeGotIP &event)
 {
   Serial.println("Connected to Wi-Fi.");
+  connectToMqtt();
 }
 
 void onWifiDisconnect(const WiFiEventStationModeDisconnected &event)
 {
   Serial.println("Disconnected from Wi-Fi.");
   wifiReconnectTimer.once(2, connectToWifi);
+}
+
+
+
+void onMqttConnect(bool sessionPresent)
+{
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
+{
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected())
+  {
+    mqttReconnectTimer.once(2, connectToMqtt);
+  }
+}
+
+void onMqttPublish(uint16_t packetId)
+{
+Serial.print("Publish acknowledged.");
+Serial.print("PacketId: ");
+Serial.println(packetId);
 }
 
 void setup()
@@ -42,6 +91,13 @@ void setup()
   dht.begin();
 
   wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
+  WiFi.onStationModeDisconnected(onWifiDisconnect);
+
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  mqttClient.setCredentials(MQTT_USERNAME, MQTT_PASSWORD);
 
   connectToWifi();
 }
