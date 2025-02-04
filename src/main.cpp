4be866acd464c1b4519efba49cf2e60fc3ce143d
Sjoerd Lemkens <97674344+sjoerdlmkns@users.noreply.github.com>
2025-02-04 19:13:52 +0100

    feat: connect to wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,38 @@
+#include <Arduino.h>
 #include <DHT.h>
+#include <ESP8266WiFi.h>
+#include <Ticker.h>
 
+// DHT Sensor
 #define DHT_BUS 4
 #define DHT_TYPE DHT11
 DHT dht(DHT_BUS, DHT_TYPE);
+
+// WIFI settings
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+// Wifi managers
+WiFiEventHandler wifiConnectHandler;
+WiFiEventHandler wifiDisconnectHandler;
+Ticker wifiReconnectTimer;
+
+void connectToWifi()
+{
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void onWifiConnect(const WiFiEventStationModeGotIP &event)
+{
+  Serial.println("Connected to Wi-Fi.");
+}
+
+void onWifiDisconnect(const WiFiEventStationModeDisconnected &event)
+{
+  Serial.println("Disconnected from Wi-Fi.");
+  wifiReconnectTimer.once(2, connectToWifi);
+}
 
 void setup()
 {
@@ -10,6 +40,10 @@ void setup()
   Serial.println("Start");
 
   dht.begin();
+
+  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
+
+  connectToWifi();
 }
 
 void loop()
